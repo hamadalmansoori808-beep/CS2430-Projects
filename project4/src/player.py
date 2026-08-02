45b149c1f07e1cbcd2264c7445084d5d88bb30e6
Hamad Almansoori <hamadalmansoori808@gmail.com>
2026-08-02 09:01:18 +0500

    Update player.py

--- project4/src/player.py
+++ project4/src/player.py
@@ -1,1 +1,29 @@
+"""
+Name: Hamad Almansoori
+Course: CS2430
 
+Programming Project 4 - Monopoly Simulation
+
+Purpose:
+Stores player information.
+"""
+
+
+class Player:
+
+
+    def __init__(self):
+
+        self.position = 0
+
+        self.in_jail = False
+
+        self.jail_attempts = 0
+
+        self.get_out_card = 0
+
+
+
+    def move(self, spaces):
+
+        self.position = (self.position + spaces) % 40
