c5458ffce1b2d76b6b191c7528248a62f3914012
Hamad Almansoori <hamadalmansoori808@gmail.com>
2026-08-02 09:01:39 +0500

    Update cards.py

--- project4/src/cards.py
+++ project4/src/cards.py
@@ -1,1 +1,76 @@
+"""
+Name: Hamad Almansoori
+Course: CS2430
 
+Programming Project 4 - Monopoly Simulation
+
+Purpose:
+Creates Chance and Community Chest decks.
+"""
+
+
+import random
+
+
+
+class Deck:
+
+
+    def __init__(self, cards):
+
+        self.cards = cards.copy()
+
+        self.discard = []
+
+        random.shuffle(self.cards)
+
+
+
+    def draw(self):
+
+        if len(self.cards) == 0:
+
+            self.cards = self.discard
+
+            self.discard = []
+
+            random.shuffle(self.cards)
+
+
+        card = self.cards.pop()
+
+        self.discard.append(card)
+
+        return card
+
+
+
+chance_cards = [
+
+    "GO",
+
+    "ILLINOIS",
+
+    "ST_CHARLES",
+
+    "JAIL",
+
+    "BACK3",
+
+    "NONE"
+
+]
+
+
+
+community_cards = [
+
+    "GO",
+
+    "JAIL",
+
+    "NONE",
+
+    "NONE"
+
+]
